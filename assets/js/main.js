3b96a091f5c76e2f2f972df7a7e54ecd22fb30b3
YJunX <xushen006@gmail.com>
2022-11-18 21:01:28 +1300

    add some JS in but not quit work

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,13 @@ const response = await fetch('https://api.unsplash.com/search/photos/?query=coff
 return await response.json()
 })()
    
+
+
+(function(){
+    $("nav > li").click(function(){
+        var $sub = $(this).children(".sub");
+        $sub.slideDown();
+        var $Othersub = $(this).siblings().children(".sub");
+        $Othersub.slideUp();
+    });
+});
